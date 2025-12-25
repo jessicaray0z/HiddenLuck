6303c201ffb4410b71e928ff8abc0855de93c01e
jessicaray0z <DenboMontelle1467@hotmail.com>
2025-12-25 14:55:53 +0800

    deploy: add LuckCoin deployment script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedLuckCoin = await deploy("LuckCoin", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`LuckCoin contract: `, deployedLuckCoin.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_luckCoin"; // id required to prevent reexecution
+func.tags = ["LuckCoin"];
